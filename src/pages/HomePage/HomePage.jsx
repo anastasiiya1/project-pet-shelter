02f19354b4ef18849b812e9aa058300c7b94670f
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-09-05 13:04:24 +0300

    remove demo from home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,3 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getDemo, getDemoAll } from '../../redux/demo/operations';
-import { selectIsLoading, selectError } from '../../redux/demo/selectors';
-// import { nanoid } from 'nanoid';
-
 import Hero from '../../components/Hero/Hero';
 import AnimalsDemo from '../../components/AnimalsDemo/AnimalsDemo';
 import AboutUs from '../../components/AboutUs/AboutUs';
@@ -14,33 +8,10 @@ import Partners from '../../components/Partners/Partners';
 import Donations from '../../components/Donations/Donations';
 import OurBlog from '../../components/OurBlog/OurBlog';
 
-
 const HomePage = () => {
-    const dispatch = useDispatch();
-    // const demo = useSelector(selectDemo);
-    // const records = useSelector(selectRecords);
-    const isLoading = useSelector(selectIsLoading);
-    const error = useSelector(selectError);
-
-    useEffect(() => {
-        dispatch(getDemo());
-        dispatch(getDemoAll());
-    }, [dispatch]);
-
-
-    if (isLoading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error}</p>;
-
     return (
         <div>
-            {/* <p>Demo requests:</p>
-            <p>{demo}</p>
-            {Array.isArray(records) && records.length > 0 ? (
-                records.map((record) => <div key={nanoid()}>{record.name}</div>)
-            ) : (
-                <p>No records found.</p>
-            )} */}
-             <Hero />
+            <Hero />
             <AnimalsDemo />
             <AboutUs />
             <OurTeam />
